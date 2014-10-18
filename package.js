fc041b1407f5edba3defc92865945f047cd58ca1
Jeremy Shimko <jshimko@cloudlinemedia.co>
2014-10-18 16:59:11 -0400

    build user profile edit page

--- package.js
+++ package.js
@@ -47,7 +47,8 @@ Package.onUse(function (api) {
     'lib/client/views/d4g_hero.html',
     'lib/client/views/d4g_hero.js',
     'lib/client/views/d4g_button_bar.html',
-    'lib/client/views/d4g_user_profile.html'
+    'lib/client/views/d4g_user_profile.html',
+    'lib/client/views/d4g_user_edit.html'
   ], 'client');
 
 
