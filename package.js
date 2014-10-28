9b03b5fffcc5183bc7b33e1cb4d694cbca14a09f
Jeremy Shimko <jshimko@cloudlinemedia.co>
2014-10-28 11:32:42 -0400

    build press page and dynamic content for it

--- package.js
+++ package.js
@@ -11,7 +11,10 @@ Package.onUse(function (api) {
   api.use([
     'telescope-lib',
     'telescope-base',
-    'telescope-search'
+    'telescope-search',
+    'telescope-tags',
+    'meteorhacks:fast-render@1.0.0',
+    'meteorhacks:subs-manager@1.1.0'
     ], ['client', 'server']);
 
 
@@ -41,6 +44,10 @@ Package.onUse(function (api) {
     // Helpers
     'lib/client/helpers/spacebars.js',
 
+    // Layouts
+    'lib/client/views/layouts/press_layout.html',
+    'lib/client/views/layouts/press_layout.js',
+
     // Templates
     'lib/client/views/main.html',
     'lib/client/views/custom_templates.js',
@@ -55,7 +62,13 @@ Package.onUse(function (api) {
     'lib/client/views/d4g_hero.html',
     'lib/client/views/d4g_button_bar.html',
     'lib/client/views/d4g_user_profile.html',
-    'lib/client/views/d4g_user_edit.html'
+    'lib/client/views/d4g_user_edit.html',
+
+    // Pages
+    'lib/client/views/pages/about.html',
+    'lib/client/views/pages/press.html',
+    'lib/client/views/pages/press.js'
+
   ], 'client');
 
 
@@ -63,6 +76,7 @@ Package.onUse(function (api) {
   *  Server
   */
   api.addFiles([
+    'lib/server/publications.js',
     'lib/server/seed.js'
   ], 'server');
 
@@ -70,6 +84,8 @@ Package.onUse(function (api) {
   /*
   *  Client & Server
   */
-
+  api.addFiles([
+    'lib/router/routes.js'
+  ], ['client', 'server']);
 
 });
