85c2c59a5fd26a4edef9a9384d106d49cb522951
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-02-02 23:06:11 -0500

    add autoprefixer to the compass workflow

--- lib/client/scss/config.rb
+++ lib/client/scss/config.rb
@@ -1,5 +1,6 @@
 require 'susy'
 require 'breakpoint'
+require 'autoprefixer-rails'
 require 'compass-notify'
 
 css_dir = "../css"
@@ -8,3 +9,19 @@ sass_dir = "."
 output_style = :expanded  # expanded for development debugging
 
 # line_comments = false
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+  end
+end
