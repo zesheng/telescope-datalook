a0bb766a9f4815cb4bd6b2d29e0a16fa79cbde76
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-05-28 11:01:36 -0400

    add post location to post items display

--- package.js
+++ package.js
@@ -86,6 +86,7 @@ Package.onUse(function (api) {
     'lib/client/views/d4g_post_content.html',
     'lib/client/views/d4g_post_tagline.html',
     'lib/client/views/d4g_post_tagline.js',
+    'lib/client/views/d4g_post_location.html',
     'lib/client/views/d4g_post_discuss.html',
     'lib/client/views/d4g_post_submit_form.html',
     'lib/client/views/d4g_post_submit_form.js',
